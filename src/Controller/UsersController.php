b1f0ec6566b8d57f79c548a876867c5110803e9b
MBELKASEM <mohamed.belkasem@gmail.com>
2023-05-04 17:37:17 +0200

    Formulaire  EditProfileUserType

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\EditProfileUserType;
 
 class UsersController extends AbstractController
 {
@@ -17,5 +19,24 @@ class UsersController extends AbstractController
             'user' => $user,
         ]);
     }
+
+    #[Route('/user/edit', name: 'user_profil_edit')]
+        public function editUserProfil(Request $request, EntityManagerInterface $em): Response
+        {
+            $user = $this->getUser();
+            $form = $this->createForm(EditProfileUserType::class, $user);
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->flush();
+                $this->addFlash('success', 'Profile updated');
+            }
+
+            return $this->render('users/edituser.html.twig', [
+                'user' => $user,
+                'form' => $form->createView(),
+            ]);
+        }
+
 }
 
