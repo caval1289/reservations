6ae56b6e620260f944b1100081786d6cdfdf982d
MBELKASEM <mohamed.belkasem@gmail.com>
2023-05-05 17:46:07 +0200

    Formulaire Modification mot de passe

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Form\EditProfileUserType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\EditProfileUserType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Form\ChangePasswordType;
+use \App\Entity\User;
+
 
 class UsersController extends AbstractController
 {
@@ -21,22 +26,55 @@ class UsersController extends AbstractController
     }
 
     #[Route('/user/edit', name: 'user_profil_edit')]
-        public function editUserProfil(Request $request, EntityManagerInterface $em): Response
+    public function editUserProfil(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileUserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Profile utilisateur mis à jour');
+        }
+
+        return $this->render('users/edituser.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+ 
+
+    #[Route('/user/changepwd', name: 'user_profil_pwd')]
+        public function editUserPassword(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher): Response
         {
             $user = $this->getUser();
-            $form = $this->createForm(EditProfileUserType::class, $user);
-
+            $form = $this->createForm(ChangePasswordType::class);
             $form->handleRequest($request);
+            
             if ($form->isSubmitted() && $form->isValid()) {
+                // encode the plain password
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
+    
+                $this->addFlash('success', 'Mot de passe mis à jour');
+                $em->persist($user);
                 $em->flush();
-                $this->addFlash('success', 'Profile updated');
+    
+                return $this->redirectToRoute('user_profil');
             }
-
-            return $this->render('users/edituser.html.twig', [
-                'user' => $user,
+    
+            return $this->render('users/pwduser.html.twig', [
                 'form' => $form->createView(),
             ]);
         }
 
 }
+
+
+
 
--- src/Form/EditProfileUserType.php
+++ src/Form/EditProfileUserType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 
 class EditProfileUserType extends AbstractType
@@ -15,14 +17,25 @@ class EditProfileUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('login', TextType::class, ['label' => 'Identifiant'])
-            ->add('firstname', TextType::class, ['label' => 'Prénom'])
-            ->add('lastname', TextType::class, ['label' => 'Nom'])
-            ->add('email', TextType::class, ['label' => 'E-mail'])   
-            ->add('language', TextType::class, ['label' => 'Langue'])
-            ->add('Valider', SubmitType::class) 
-            
-        ;
+        ->add('login', TextType::class, ['label' => 'Identifiant'])
+        ->add('firstname', TextType::class, ['label' => 'Prénom'])
+        ->add('lastname', TextType::class, ['label' => 'Nom'])
+        ->add('email', TextType::class, ['label' => 'E-mail'])
+        ->add('language', ChoiceType::class, [
+            'label' => 'Langue',
+            'choices' => [
+                'Anglais' => 'en',
+                'Français' => 'fr'               
+            ],
+            'expanded' => false,
+            'multiple' => false,
+            'attr' => [
+                'class' => 'form-control'
+            ]
+        ])
+        ->add('Valider', SubmitType::class)
+    ;
+    
     } 
 
     public function configureOptions(OptionsResolver $resolver): void
